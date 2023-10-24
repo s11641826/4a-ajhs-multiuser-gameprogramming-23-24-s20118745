21296db26f1adfa85d8d4ce7b2c38ddf06a4ffa1
s20118745 <s20118745@students.duvalschools.org>
2023-10-24 14:40:05 -0400

    Yeah!

--- multiUserGameProgramming/programTemplate.cs
+++ multiUserGameProgramming/programTemplate.cs
@@ -5,8 +5,60 @@ namespace UpdateForEachProgram
 {
     class UpdateForEachProgram
     {
+
+
+
+        static void MyMethod()
+        {
+            Console.WriteLine("I Like Mine with lettuce and tomatoes, Heinz 57, and french fried potatoes.\n");
+        }
+        // static -- This method belongs to the current class, it is NOT an object
+        // void -- This method has no return value.
+
+        static int DoubleUp()
+        {
+            int sum = 0;
+            Console.WriteLine("This method will double a number and return it.\n");
+            Console.WriteLine("Please enter a number on the next line.\n");
+            sum = System.Convert.ToInt32(Console.ReadLine());
+            //sum *= 2;
+            //Console.WriteLine(sum);
+            return sum *= 2;
+        }
+
+        // Mthods with Parameters
+
+        static void MakePancakes(int num)
+        {
+            for (int i = 0; i < num; i++)
+            {
+                Console.WriteLine("One golden, fluffy pancake coming up1\n");
+            }
+        }
+
+
+
+        static void Main(string[] args
+        {
+
+            //MyMethod();
+            //DoubleUp();
+            MakePancakes();
+            
+        }
+        
+        
+        
+        )
+
+                return 0;
+            
+        }
+
+
         static void Main(string[] args)
         {
+            MyMethod();
 
         }
     }
