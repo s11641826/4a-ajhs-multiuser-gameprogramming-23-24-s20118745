ec58056b0cb70d06a52ec534d3d7e02a385df20d
s20118745 <s20118745@students.duvalschools.org>
2023-11-14 13:49:02 -0500

    yipee

---
+++ multiUserGameProgramming/5a_exampleGameMethods/programTemplate.cs
@@ -0,0 +1,13 @@
+// 02_Collections, Alexander Jones, 11-08-2023 v0.0
+using System;
+
+namespace PRogram
+{
+    class Program
+    (
+        static void Main(string[] args)
+        }
+
+        }
+    }
+}
